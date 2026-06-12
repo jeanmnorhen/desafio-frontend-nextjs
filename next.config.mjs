0a9abfc96d50bbc9dea39c5ce35c5d6630c6ba08
jeanmnorhen <jeanmdasleite@gmail.com>
2026-06-12 16:20:45 -0300

    fix next

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,7 @@ const withSerwist = withSerwistInit({
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
   disable: process.env.NODE_ENV === "development", // Desativar em dev para não cachear HMR
+  reloadOnOnline: false,
 });
 
 export default withSerwist(nextConfig);
